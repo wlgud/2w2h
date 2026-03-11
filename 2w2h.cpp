5d44f76aa1eba573366100ddbd6c4493538e6a91
박지형 <pjh82381425@gmail.com>
2026-03-11 20:17:24 +0900

    Add files via upload

---
+++ 2w2h.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I LOVE C++" << endl;
+    cout << endl;
+
+    cout << "I       L        OOO       V     V      EEEEE" << endl;
+    cout << "I       L       O   O       V   V       E" << endl;
+    cout << "I       L       O   O        V V        EEEE" << endl;
+    cout << "I       L       O   O         V         E" << endl;
+    cout << "I       LLLLL    OOO          V         EEEEE" << endl;
+
+    cout << endl;
+
+    cout << "  CCCC        ++     ++" << endl;
+    cout << " C            ++     ++" << endl;
+    cout << " C          ++++++ ++++++" << endl;
+    cout << " C            ++     ++" << endl;
+    cout << "  CCCC        ++     ++" << endl;
+
+    return 0;
+}
